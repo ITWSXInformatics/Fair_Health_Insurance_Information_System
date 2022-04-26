27b9fa2f0c90bb0eed083b24b85f1ed32c29bfd0
Brian Hotopp <brihoto@gmail.com>
2022-04-26 09:48:00 -0400

    added validation to form page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,7 @@
 import { reactive } from 'vue'
 export const store = reactive({
-someValue: "here is some global state available to every component",
-incomeBracket: "Not Selected",
-age: "Not Selected",
-dependents: "- ",
-gender:"Not Selected"
+incomeBracket: null,
+ageBracket: null,
+dependents: null,
+gender: null
 })
