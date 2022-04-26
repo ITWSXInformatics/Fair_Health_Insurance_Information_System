ce6cd53b9202bb81c196e88a938e55967138421a
Peter Ly <36087674+WindLark@users.noreply.github.com>
2022-04-26 09:10:10 -0400

    Templating Commit

--- frontend/src/components/Results.vue
+++ frontend/src/components/Results.vue
@@ -10,12 +10,32 @@ const count = ref(0)
 
 <template>
 <div>
-  this content is from inside the results page
+  <h1><b>Results</b></h1>
+        <div class="home-text">
+          
+        </div>
 </div>
 </template>
 
 <style scoped>
 a {
   color: #42b983;
+}
+</style>
+
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+.home-text {
+  background: white;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.05);
+  border-radius: 10px;
+  padding-left: 3%;
+  padding-right: 3%;
+  padding-top: 2%;
+  padding-bottom: 2%;
 }
 </style>
